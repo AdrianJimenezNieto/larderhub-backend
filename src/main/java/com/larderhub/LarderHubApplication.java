364237f485ea06112b991e2983745b209180a665
Adrián Jiménez <adrianjimeneznieto@gmail.com>
2026-01-15 13:50:46 +0100

    chore(init): initial spring boot project structure

---
+++ src/main/java/com/larderhub/LarderHubApplication.java
@@ -0,0 +1,13 @@
+package com.larderhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarderHubApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LarderHubApplication.class, args);
+  }
+
+}
